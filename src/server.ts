02c3d3a1a313418e158933d2f1434149681a6ff4
matheussanchez <matheus.sanchez@ifood.com.br>
2021-08-05 22:44:45 -0300

    Adding index to routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
 
-import { categoriesRouter } from './routes/categories.routes';
-import { specificationRouter } from './routes/specifications.routes';
+import { router } from './routes/index';
 
 const app = express();
 app.use(express.json());
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hellow World !' });
-});
-
-app.use('/categories', categoriesRouter);
-app.use('/specification', specificationRouter);
+app.use(router);
 
 app.listen(3333, () => {
   console.log('API Running !');
