bc19ef605e935ee0f4e3047a66ca081efa5482fe
matheussanchez <matheus.sanchez@ifood.com.br>
2021-08-16 17:01:12 -0300

    Starting Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes/index';
+import swaggerFile from './swagger.json';
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 app.use(router);
 
 app.listen(3333, () => {
-  console.log('API Running !');
+  console.log('API Running on 3333 !');
 });
